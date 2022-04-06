c6c8ea01c284a296227f10d98a885cff8121ba96
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2022-04-06 21:04:21 +0300

    reset latest commit

--- main_test.go
+++ main_test.go
@@ -17,7 +17,7 @@ func TestIsValidNumber(t *testing.T) {
 		{num: "123AB", base: 10, want: false},
 		{num: "", base: 10, want: true},
 		{num: "000", base: 0, want: false},
-		{num: "000", base: 1, want: true}, // i found only theories about base 0 or 1, but noone use them in any practical case, so i think false here will be better
+		{num: "000", base: 1, want: false}, // i found only theories about base 0 or 1, but noone use them in any practical case, so i think false here will be better
 	} {
 		if got := isValidNumber(tc.num, tc.base); got != tc.want {
 			t.Errorf("isValidNumber(%v, %v) = (%v), want = (%v)", tc.num, tc.base, got, tc.want)
